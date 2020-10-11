7cfb22f5c17308f7d591f728853dff987e8f9fbd
Donz2020 <donoval.candolfi@studenti.unicam.it>
2020-10-11 14:06:03 +0200

    test commit signature

--- it_unicam_cs_ids_justmeet_frontend/src/app/auth.service.ts
+++ it_unicam_cs_ids_justmeet_frontend/src/app/auth.service.ts
@@ -12,6 +12,7 @@ import {NavController} from '@ionic/angular';
 export class AuthService {
 
   private readonly jwtTokenName = 'backend.app.jwtSecret';
+  
 
   private authUser = new ReplaySubject<string | null>(1);
   public authUserObservable = this.authUser.asObservable();
