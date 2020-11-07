a07802651b638d04b64950f10b962cfc7eda5cd2
ArlindCanga <50328762+ArlindCanga@users.noreply.github.com>
2020-11-07 16:05:49 +0100

    Fix setPassword

--- it_unicam_cs_ids_justmeet_frontend/JustmeetFrontend/src/app/settings/settings.component.ts
+++ it_unicam_cs_ids_justmeet_frontend/JustmeetFrontend/src/app/settings/settings.component.ts
@@ -2,25 +2,22 @@ import { Component, OnInit } from '@angular/core';
 
 import { TokenStorageService } from '../_services/token-storage.service';
 import {UserService} from "../_services/user.service";
-import {Observable} from "rxjs";
 import {settingsPayload} from "./settingsPayload";
 import {FormControl, FormGroup} from "@angular/forms";
 import {AppComponent} from "../app.component";
 import {Router} from "@angular/router";
-
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.css']
 })
+
+
 export class SettingsComponent implements OnInit {
   currentUser: string;
   userDetails : settingsPayload;
-  changePasswordForm: FormGroup;
-  form: any = {};
-  values = '';
-
+  formSettings : FormGroup;
 
   constructor(private token: TokenStorageService, private userService: UserService, private logoutComponent : AppComponent, private router : Router) { }
 
@@ -31,30 +28,20 @@ export class SettingsComponent implements OnInit {
       allData = JSON.stringify(data);
       this.userDetails = JSON.parse(allData);
     });
-    //this.changePasswordForm.controls.proof.patchValue('');
+    this.initForm();
   }
 
-/*
-  changePassword(){
-    //this.currentUser = this.token.getUser();
-    this.changePasswordForm = new FormGroup({
-      pass: new FormControl()
-    })
-   this.userService.setUserPass(this.changePasswordForm.value);
-  }
-*/
+  initForm(): void{
+    this.formSettings = new FormGroup({
+      newPass: new FormControl(''),
+    });
+  };
 
   onSubmit() {
-    /*
-    this.changePasswordForm = new FormGroup({
-      pass: new FormControl()
-    });
-    */
-    alert(this.values);
-    this.userService.setUserPass(this.values);
-
-
+    this.userService.setUserPass(this.formSettings.get('newPass').value).subscribe();
   }
+
+
 
   deleteAccount() {
 
