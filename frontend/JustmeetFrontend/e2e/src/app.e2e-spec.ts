2baf8ee900b40328d1cba6870311f053b23aa775
Donz2020 <donoval.candolfi@studenti.unicam.it>
2020-11-13 16:17:08 +0100

    modified TODO

--- frontend/JustmeetFrontend/e2e/src/app.e2e-spec.ts
+++ frontend/JustmeetFrontend/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Angular8JwtAuth app is running!');
+    expect(page.getTitleText()).toEqual('JustMeetFrontend app is running!');
   });
 
   afterEach(async () => {
--- frontend/JustmeetFrontend/src/app/app.component.spec.ts
+++ frontend/JustmeetFrontend/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Angular8JwtAuth'`, () => {
+  it(`should have as title 'JustMeetFrontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Angular8JwtAuth');
+    expect(app.title).toEqual('JustMeetFrontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Angular8JwtAuth app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('JustMeetFrontend app is running!');
   });
 });
