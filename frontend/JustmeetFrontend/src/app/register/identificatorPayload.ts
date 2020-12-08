6e800fa710b91887309723d6954417d4ee1b596d
ArlindCanga <arlind.canga@studenti.unicam.it>
2020-12-08 12:05:20 +0100

    Signup payload modified

---
+++ frontend/JustmeetFrontend/src/app/register/identificatorPayload.ts
@@ -0,0 +1,4 @@
+export interface idPayload {
+  username: string;
+  password: string;
+}
--- frontend/JustmeetFrontend/src/app/register/register.component.ts
+++ frontend/JustmeetFrontend/src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+/*import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../_services/auth.service';
 
 @Component({
@@ -30,5 +30,79 @@ export class RegisterComponent implements OnInit {
         this.isSignUpFailed = true;
       }
     );
+  }
+}*/
+
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from '../_services/auth.service';
+import {FormControl, FormGroup} from "@angular/forms";
+import {idPayload} from "./identificatorPayload";
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+  isSignUpFailed = false;
+  isSuccessful = false;
+  errorMessage = '';
+  formSignup : FormGroup;
+  submitted : boolean = true;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.initForm();
+  }
+
+  initForm(): void {
+    this.submitted = false;
+    this.formSignup = new FormGroup({
+      email: new FormControl(''),
+      pass: new FormControl(''),
+      name: new FormControl(''),
+      surname: new FormControl(''),
+      birthDate: new FormControl(''),
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.formSignup.valid) {
+      let idPayload : idPayload = {
+        username: this.formSignup.get('email').value,
+        password: this.formSignup.get('pass').value,
+      };
+      this.authService.register(idPayload).subscribe(
+        (data : idPayload ) => {
+          data = idPayload;
+          console.log(data);
+          this.isSignUpFailed = false;
+          this.isSuccessful = true;
+          window.location.href = "/login";
+        },
+        err => {
+          this.errorMessage = err.error.message;
+          this.isSignUpFailed = true;
+        }
+      );
+    };
+  }
+  keyDownFunction(event){
+    if (this.submitted){
+      this.submitted = false;
+    }
+    if (event.keyCode === 13) {
+      this.setSubmitted();
+      this.formSignup.validator;
+      if (this.formSignup.validator){
+        this.onSubmit();
+      }
+    }
+  }
+
+  setSubmitted(){
+    this.submitted = true;
   }
 }
