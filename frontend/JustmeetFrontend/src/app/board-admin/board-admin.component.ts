7e9ab127a52dee78a7d9b3a1e9d43424de91e371
Donz2020 <donoval.candolfi@studenti.unicam.it>
2021-01-06 18:31:58 +0100

    check admin or mod when display admin board

--- frontend/JustmeetFrontend/src/app/board-admin/board-admin.component.ts
+++ frontend/JustmeetFrontend/src/app/board-admin/board-admin.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { UserService } from '../_services/user.service';
+import {Component, OnInit} from '@angular/core';
+import {UserService} from '../_services/user.service';
 import {TokenStorageService} from "../_services/token-storage.service";
 import {profilePayload} from "../utils/profilePayloads/profilePayload";
 import {Router} from "@angular/router";
 import {AdminService} from "../_services/admin.service";
+import {FormControl, FormGroup} from "@angular/forms";
+import {of} from "rxjs";
 
 @Component({
   selector: 'app-board-admin',
@@ -13,18 +15,43 @@ import {AdminService} from "../_services/admin.service";
 export class BoardAdminComponent implements OnInit {
   userDetails: profilePayload;
   currentUser: string;
+  formAdmin : FormGroup;
+  checkAdmin : boolean = false;
+  checkMod : boolean = false;
 
 
-constructor(private token: TokenStorageService, private userService: UserService,private router: Router,private adminService: AdminService) { }
+  constructor(private token: TokenStorageService, private userService: UserService, private router: Router, private adminService: AdminService) {
+  }
 
-ngOnInit() {
-  let allData: string;
-  this.currentUser = this.token.getUser();
-  this.userService.getUserDetails().subscribe((data : profilePayload)=>{
-    allData = JSON.stringify(data);
-    this.userDetails = JSON.parse(allData);
-  });
-}
+  ngOnInit() {
+    let allData: string;
+    this.currentUser = this.token.getUser();
+    this.userService.getUserDetails().subscribe((data: profilePayload) => {
+      allData = JSON.stringify(data);
+      this.userDetails = JSON.parse(allData);
+      for(let i of this.userDetails.roles){
+        if(i == "ADMIN" ){
+          this.checkAdmin = true;
+        }
+        if (i =="MOD"){
+          this.checkMod = true;
+        }
+      }
+
+    });
+    this.initForm();
+  }
+
+  initForm(): void {
+
+    this.formAdmin = new FormGroup({
+      email: new FormControl(''),
+      pass: new FormControl(''),
+      name: new FormControl(''),
+      surname: new FormControl(''),
+      birthDate: new FormControl(''),
+    });
+  }
 
 
   deleteAccount() {
